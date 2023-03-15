d3ca689e313bf1f70facf86d54388cad1713677c
N4choWasTaken <thierrygmuer@outlook.com>
2023-03-15 14:02:14 +0100

    :truck: moved login files to com.thierry.whatsdown.login package

--- app/src/main/java/com/thierry/whatsdown/login/LoginActivity.kt
+++ app/src/main/java/com/thierry/whatsdown/login/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.thierry.whatsdown
+package com.thierry.whatsdown.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thierry.whatsdown.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
