273c7f52def2cac0b65a5c909e883ff6ddd77ea0
N4choWasTaken <thierrygmuer@outlook.com>
2023-03-16 14:23:44 +0100

    :sparkles: Messages are shown in the chat

--- app/src/main/java/com/thierry/whatsdown/DmActivity.kt
+++ app/src/main/java/com/thierry/whatsdown/DmActivity.kt
@@ -1,11 +1,36 @@
 package com.thierry.whatsdown
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.thierry.whatsdown.chats.Chat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class DmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dm)
+
+
+        GlobalScope.launch(Dispatchers.Main) {
+
+            var messagesFromChat =
+                Chat.getMessagesFromChat(intent.getSerializableExtra("chatId").toString())
+            val layout = findViewById<LinearLayout>(R.id.messages)
+
+
+            for (i in messagesFromChat.indices) {
+                val textView = TextView(this@DmActivity)
+                textView.text = messagesFromChat[i].content
+                layout.addView(textView)
+            }
+        }
     }
+
 }
--- app/src/main/java/com/thierry/whatsdown/chats/Chat.kt
+++ app/src/main/java/com/thierry/whatsdown/chats/Chat.kt
@@ -1,8 +1,34 @@
 package com.thierry.whatsdown.chats
 
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
 import com.thierry.whatsdown.User
+import com.thierry.whatsdown.chats.messages.Messages
+import com.thierry.whatsdown.database.DataBase
+import kotlinx.coroutines.tasks.await
 
 class Chat(_user1: User, _user2: User) {
     var user1: User = _user1
     var user2: User = _user2
+
+
+    companion object {
+        suspend fun getMessagesFromChat(chatId: String): List<Messages> {
+            val db = DataBase.connect()
+            val chatRef = db.collection("chats").document(chatId)
+
+            val dbMessages = db.collection("messages")
+                .whereEqualTo("chat", chatRef)
+                .get()
+                .await()
+
+            val messages = dbMessages.documents.map { doc ->
+                val user = doc.get("user").toString() ?: ""
+                val content = doc.get("content").toString() ?: ""
+                Messages(content, chatRef, user)
+            }
+
+            return messages
+        }
+    }
 }
