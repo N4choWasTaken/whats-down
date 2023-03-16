cf7393231889fec4a64346ad58ac9a6786ff1389
N4choWasTaken <thierrygmuer@outlook.com>
2023-03-16 08:39:59 +0100

    :lock: user can signin / newly learned: suspendCancellableCoroutine

--- app/src/main/java/com/thierry/whatsdown/login/LoginActivity.kt
+++ app/src/main/java/com/thierry/whatsdown/login/LoginActivity.kt
@@ -3,10 +3,13 @@ package com.thierry.whatsdown.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.thierry.whatsdown.R
+import com.thierry.whatsdown.database.DataBase
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        DataBase.connect()
     }
 }
