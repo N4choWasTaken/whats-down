e8028011854d5ec64ef818b4beb5d681e2f2387a
N4choWasTaken <thierrygmuer@outlook.com>
2023-03-17 00:23:30 +0100

    :sparkles: user can write messages

--- app/src/main/java/com/thierry/whatsdown/chats/messages/Messages.kt
+++ app/src/main/java/com/thierry/whatsdown/chats/messages/Messages.kt
@@ -1,9 +1,10 @@
 package com.thierry.whatsdown.chats.messages
 
 import com.google.firebase.firestore.DocumentReference
+import com.thierry.whatsdown.User
 
-class Messages(_content: String, _chat: DocumentReference, _user: String) {
+class Messages(_content: String, _chat: DocumentReference, _user: DocumentReference) {
     var content = _content
-    var user = _chat
-    var chat = _user
+    var user = _user
+    var chat = _chat
 }
